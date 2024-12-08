540a65aa69b204eb9f1fdd1c10ade87e8c3ce891
Sharif Neto <sharifneto@gmail.com>
2024-12-08 23:51:45 +0000

    Dimensão produtos criada

---
+++ models/staging/erp/stg_erp__fornecedores.sql
@@ -0,0 +1,11 @@
+with
+    fonte_fornecedores as (
+        select
+            cast(ID as int) as pk_fornecedor
+            , cast(COMPANYNAME as varchar) as nome_fornecedor
+            , cast(CITY as varchar) as cidade_fornecedor
+            , cast(COUNTRY as varchar) as pais_fornecedor
+        from {{source("erp",'suppliers')}}
+    )
+select *
+from fonte_fornecedores
